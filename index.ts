c824e458f58b428514c29755d45f63d989ffa479
PlatMediaProjects <info@platinumhitsentertainment.com>
2025-05-17 21:48:39 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -72,18 +72,3 @@ app.use((req, res, next) => {
     log(`serving on port ${port}`);
   });
 })();
-import express from 'express';
-import path from 'path';
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve('public', 'index.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
