7c8684a03249bd78f7303c60fb14280e57029257
PlatMediaProjects <info@platinumhitsentertainment.com>
2025-05-23 02:39:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,5 +12,18 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const http = require('http');
+
+app.get('/stream', (req, res) => {
+  const streamUrl = 'http://cast3.my-control-panel.com:7535/;stream.mp3'; // Asura HTTP stream
+
+  http.get(streamUrl, (streamRes) => {
+    res.setHeader('Content-Type', 'audio/mpeg');
+    streamRes.pipe(res);
+  }).on('error', (err) => {
+    console.error('Stream Error:', err.message);
+    res.status(500).send('Stream Unavailable');
+  });
+});
 
 
