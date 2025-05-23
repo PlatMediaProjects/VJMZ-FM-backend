55b041eacb1f796f44e91dae1d5eda4a52c1cc44
PlatMediaProjects <info@platinumhitsentertainment.com>
2025-05-23 03:22:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,18 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-const http = require('http');
 
-app.get('/stream', (req, res) => {
-  const streamUrl = 'http://cast3.my-control-panel.com:7535/;stream.mp3';
-
-  http.get(streamUrl, (streamRes) => {
-    res.setHeader('Content-Type', 'audio/mpeg');
-    streamRes.pipe(res);
-  }).on('error', (err) => {
-    console.error('Stream Error:', err.message);
-    res.status(500).send('Stream Unavailable');
-  });
 });
 
 
