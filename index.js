b4480211be9f0c8b7eb8b81385136316f8c950b8
PlatMediaProjects <info@platinumhitsentertainment.com>
2025-05-23 01:22:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Serve static files (like your HTML homepage) from "public" directory
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  ');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
